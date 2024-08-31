d91d5f62dad031a420b1870c9a956a0159024510
Mahfujur Rahman <mahfujurrahman6793@gmail.com>
2024-08-31 23:34:11 +0600

    added preloader

--- resources/views/pages/project-details-light.blade.php
+++ resources/views/pages/project-details-light.blade.php
@@ -36,21 +36,7 @@
   <div class="body-overlay"></div>
 
   <!-- Preloader -->
-  <div id="preloader">
-    <div id="container" class="container-preloader">
-      <div class="animation-preloader">
-        <div class="spinner"></div>
-        <div class="txt-loading">
-          <span data-text="I" class="characters">I</span>
-          <span data-text="N" class="characters">N</span>
-          <span data-text="F" class="characters">F</span>
-          <span data-text="O" class="characters">O</span>
-        </div>
-      </div>
-      <div class="loader-section section-left"></div>
-      <div class="loader-section section-right"></div>
-    </div>
-  </div>
+  @include('Layout.frontend.partial.preloader')
 
   <!-- Cursor Animation -->
   <div class="cursor1"></div>
