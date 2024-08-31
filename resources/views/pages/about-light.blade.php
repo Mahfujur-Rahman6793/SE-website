1ded0537a2d7a78fca126c62183a8d2dd4881aa0
Mahfujur Rahman <mahfujurrahman6793@gmail.com>
2024-08-31 23:40:00 +0600

    added footer

--- resources/views/pages/about-light.blade.php
+++ resources/views/pages/about-light.blade.php
@@ -343,67 +343,7 @@
         </main>
 
         <!-- Footer section start -->
-        <footer>
-          <div class="intDesigner-footer__area pt-100 pb-100">
-            <div class="container">
-              <div class="intDesigner-footer__widgets-wrapper">
-                <div class="footer__widget">
-                  <div class="intDesigner-footer__logo">
-                    <a href="index-2.html">
-                      <img src="../assets/imgs/interior-designer/logo.png" class="show-dark" alt="Site Logo">
-                      <img src="../assets/imgs/interior-designer/logo-dark.png" class="show-light" alt="Site Logo">
-                    </a>
-                  </div>
-                </div>
-                <div class="footer__widget">
-                  <div class="intDesigner-footer__menu">
-                    <ul>
-                      <li><a href="#">About Me</a></li>
-                      <li><a href="#">Work</a></li>
-                      <li><a href="#">Contact</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="footer__widget">
-                  <div class="intDesigner-footer__menu">
-                    <ul>
-                      <li><a href="#">6541, Brand tower,</a></li>
-                      <li><a href="#">New York, USA</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="footer__widget">
-                  <div class="intDesigner-footer__menu">
-                    <ul>
-                      <li><a href="mailto:info@example.com">info@example.com</a></li>
-                      <li><a href="tel:+(08)365301710">+(08) 365 301 710</a></li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="intDesigner-copyright__area">
-            <div class="container">
-              <div class="intDesigner-copyright__inner">
-                <div class="intDesigner-copyright__text">
-                  <p>© Alrights reserved by <a href="https://crowdyflow.com/">CrowdyFlow</a></p>
-                </div>
-                <div class="intDesigner-copyright__social">
-                  <a href="#">Facebook</a><a href="#">Twitter</a><a href="#">Instagram</a>
-                </div>
-                <div class="intDesigner__go-top">
-                  <a href="#" class="go-top__btn">
-                    Go Top <div class="cf_image"><img src="../assets/imgs/interior-designer/arrow-up.png"
-                        class="show-dark" alt="image"><img src="../assets/imgs/interior-designer/arrow-up-dark.png"
-                        class="show-light" alt="image">
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
+        @include('Layout.frontend.partial.footer')
         <!-- Footer section end -->
 
       </div>
